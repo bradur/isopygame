a0fc44e3718f89f00455485d52df6a1998d954f3
bradur <bradurdung@gmail.com>
2013-04-01 10:34:03 +0300

    Edited main.py to follow PEP8

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
 import pygame
-import random
 import json
 
 from loaders import *
 from classes import *
 from pygame.locals import *
 
-if not pygame.font: print 'Warning, fonts disabled'
-if not pygame.mixer: print 'Warning, sound disabled'
+if not pygame.font:
+    print 'Warning, fonts disabled'
+if not pygame.mixer:
+    print 'Warning, sound disabled'
 
 WIDTH, HEIGHT = 1920, 1080
-DELAY     = 500             #Delay when pressing keys
-INTERVAL  = 100
-FPS = 60 #change according to what the game needs. 60 is a common hz for screens
-SIZE = WIDTH, HEIGHT #Screen size
-TITLE = 'Isometric'      #Game window title
+DELAY = 500                 # Delay when pressing keys
+INTERVAL = 100
+FPS = 60                    # change according to what the game needs.
+                            # 60 is a common hz for screens
+SIZE = WIDTH, HEIGHT        # Screen size
+TITLE = 'Isometric'         # Game window title
 FONT = 'morpheus.ttf', 60
 TILE_W, TILE_H = 40, 26
 
@@ -26,39 +28,37 @@ COLOURS = {
     'hover': (250, 100, 25)
 }
 
-"""Audio settings"""
-FREQUENCY = 44100           #Audio CD quality
-BITSIZE   = -16             #Unsigned 16 bit
-CHANNEL   = 2               #1 is mono, 2 is stereo
-BUFFER    = 2048            #Number of samples (experiment to get right sound)
+FREQUENCY = 44100           # Audio CD quality
+BITSIZE = -16               # Unsigned 16 bit
+CHANNEL = 2                 # 1 is mono, 2 is stereo
+BUFFER = 2048               # Number of samples (experiment to get right sound)
 
-""" Game class """
-class Game(object):
+
+class Game(object):         # Game class
 
     def __init__(self):
 
-        pygame.mixer.pre_init(FREQUENCY, BITSIZE, CHANNEL, BUFFER)  # pre_init makes sounds work
+        pygame.mixer.pre_init(FREQUENCY, BITSIZE, CHANNEL, BUFFER)  # sounds
         pygame.init()
-        pygame.display.set_caption(TITLE)                           # Set app name
-        pygame.key.set_repeat(DELAY, INTERVAL)                      #
+        pygame.display.set_caption(TITLE)  # Set app name
+        pygame.key.set_repeat(DELAY, INTERVAL)
 
-        info = pygame.display.Info()                                # This block of code
-        xpos = info.current_w/2-WIDTH/2                             # positions the application
-        ypos = info.current_h/2-HEIGHT/2                            # to the middle of the current
-        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (xpos, ypos) # screen when initialized.
+        info = pygame.display.Info()  # get display info
+        xpos = info.current_w/2-WIDTH/2  # get screen width
+        ypos = info.current_h/2-HEIGHT/2  # get screen height
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (xpos, ypos)  # center
 
-        self.screen = pygame.display.set_mode((SIZE))               # Set screen size
-
+        self.screen = pygame.display.set_mode((SIZE))  # Set screen size
 
         self.movekeys = {
-            97:(1, 0),     # a
-            100:(-1, 0),     # d
-            115:(0, -1),     # s
-            119:(0, 1),    # w
-            273:(0, 1),     # up
-            274:(0, -1),    # down
-            275:(1, 0),     # right
-            276:(-1, 0)     # left
+            97:  (1, 0),     # a
+            100: (-1, 0),    # d
+            115: (0, -1),    # s
+            119: (0, 1),     # w
+            273: (0, 1),     # up
+            274: (0, -1),    # down
+            275: (1, 0),     # right
+            276: (-1, 0)     # left
         }
 
         self.font = pygame.font.Font('fonts/'+FONT[0], FONT[1])
@@ -68,11 +68,16 @@ class Game(object):
 
         self.centerx, self.centery = 0, 0
 
-        with open('maps/test2.json') as data_file:    
+        with open('maps/test2.json') as data_file:
             self.data = json.load(data_file)
         self.animated = [0, 0, 1, 0, 0]
-        self.texture = load_sliced_sprites(TILE_W+12, TILE_H, self.data["texture"], self.animated) #load textures'
-        
+        self.texture = load_sliced_sprites(
+            TILE_W+12,             # width
+            TILE_H,                # height
+            self.data["texture"],  # texture file
+            self.animated          # list of animated sprites
+        )  # load textures'
+
         print self.texture
 
         self.background = pygame.Surface(SIZE)
@@ -86,23 +91,26 @@ class Game(object):
         self.musicplayer = MusicPlayer()
         self.player = pygame.sprite.GroupSingle()
         self.titletext = pygame.sprite.GroupSingle()
-        self.titletext.add(Text( WIDTH/2, 0, self.font, TITLE, centering=True) ) 
+        self.titletext.add(Text(
+            WIDTH/2,                # x position
+            0,                      # y position
+            self.font,              # font object
+            TITLE,                  # text string
+            centering=True          # center text or not
+        ))
         pygame.display.flip()
 
     """ main function """
     def main(self):
-        pygame.mixer.music.play()           #Start playing music
-        
-
-        #self.player.add(Player(pygame.Rect(), ""))
+        pygame.mixer.music.play()           # Start playing music
 
         while True:
-            self.musicplayer.next()         #See if playback is finished or not
-            self.input()                    #Get user input. Mouse clicks, movements and keyboard presses.
+            self.musicplayer.next()         # See if playback is finished
+            self.input()                    # Get user input.
             self.logic()
             self.ground.update(pygame.time.get_ticks())
-            self.draw()                     #Draw sprites
-            pygame.time.Clock().tick(FPS)   #Sets the fps of the game
+            self.draw()                     # Draw sprites
+            pygame.time.Clock().tick(FPS)   # Sets the fps of the game
 
     def logic(self):
         """game logic"""
@@ -111,34 +119,42 @@ class Game(object):
         if self.state == 0:
             t = self.titletext.sprite.delay(1000/FPS)
             if t:
-                if self.titletext.sprite.rect.top < HEIGHT/2-(self.titletext.sprite.rect.height-self.titletext.sprite.rect.height/4):
+                rect = self.titletext.sprite.rect
+                title_pos = rect.height-rect.height-rect.height/4
+                if self.titletext.sprite.rect.top < HEIGHT/2-(title_pos):
                     self.titletext.sprite.move(0, 1)
 
-    #"texture": 2 def draw_rest(self, move):
-
-
     def draw_map(self, move):
-        self.centerx += move[0] #x-position of player (center of the screen)
-        self.centery += move[1] #y-position of player (center of the screen)
+        self.centerx += move[0]  # x-position of player (center of the screen)
+        self.centery += move[1]  # y-position of player (center of the screen)
         rows = self.data["rows"]
         texture = self.texture
         self.ground.empty()
         textmap = []
         tilemap = []
 
-        horizontal_tiles = WIDTH/TILE_W         #calculate amount of horizontal tiles to draw
-        vertical_tiles = (HEIGHT/TILE_H+2)/2    #calculate amount of vertical tiles to draw
-        x, y = 0, vertical_tiles*2              #iterators for the dictionaries
+        horizontal_tiles = WIDTH/TILE_W         # amount of x tiles to draw
+        vertical_tiles = (HEIGHT/TILE_H+2)/2    # amount of y tiles to draw
+        x, y = 0, vertical_tiles*2              # iterators for dicts
 
-        for i in xrange(self.centery+vertical_tiles, self.centery-vertical_tiles, -1):
+        for i in xrange(
+            self.centery+vertical_tiles,        # bottom border
+            self.centery-vertical_tiles,        # top border
+            -1                                  # reversed direction
+        ):
             line = ""
             row = []
             tileID = []
-            for j in xrange( self.centerx - horizontal_tiles, self.centerx + horizontal_tiles ):
-                x_pos = x * TILE_W - ( vertical_tiles*2-y )*13 - ( horizontal_tiles/4 * TILE_W )
-                y_pos = HEIGHT-( y - 1 )*TILE_H
+            for j in xrange(
+                self.centerx - horizontal_tiles,  # left border
+                self.centerx + horizontal_tiles   # right border
+            ):
+                vert = (vertical_tiles*2-y)*13      # vertical offset
+                hori = horizontal_tiles/4 * TILE_W  # horizontal offset
+                x_pos = x * TILE_W - vert - hori
+                y_pos = HEIGHT-(y - 1)*TILE_H
 
-                image = self.emptytile
+                image = self.emptytile  # empty tile if no tile found
                 item = "."
                 im = -1
                 iddd = None
@@ -154,11 +170,11 @@ class Game(object):
                             item = rows[i]["default_texture"]
                             im = item
                             image = texture[item]
-                        iddd = (i,j)
+                        iddd = (i, j)
 
                 tileID.append(iddd)
 
-                if i == self.centery and j == self.centerx: 
+                if i == self.centery and j == self.centerx:
                     #print j, i, self.centerx, self.centery
                     image = texture[0]
                     im = 0
@@ -167,7 +183,12 @@ class Game(object):
                 line += str(item)
                 if im >= 0:
                     if self.animated[im]:
-                        self.ground.add(AnimatedTile(x_pos, y_pos, i, j, image))
+                        self.ground.add(AnimatedTile(
+                            x_pos,
+                            y_pos,
+                            i, j,
+                            image
+                        ))
                     else:
                         self.ground.add(Tile(x_pos, y_pos, i, j, image))
                 else:
@@ -209,14 +230,42 @@ class Game(object):
 
     def options_menu(self):
         self.menu.empty()
-        self.menu.add(MenuItem( WIDTH/2, 130, self.font, "Back", "main_menu", hovered=True ))
+        self.menu.add(MenuItem(
+            WIDTH/2, 130,       # x & y positions
+            self.font,          # font object
+            "Back",             # button text
+            "main_menu",        # button target
+            hovered=True        # button is initially in hovered state
+        ))
 
     def main_menu(self):
         self.menu.empty()
-        self.titletext.add(Text( WIDTH/2, 65, self.font, TITLE, COLOURS['text'], centering=True))
-        self.menu.add(MenuItem( WIDTH/2, 130, self.font, "Start", "start_game", hovered=True ))
-        self.menu.add(MenuItem( WIDTH/2, 195, self.font, "Options", "options_menu" ))
-        self.menu.add(MenuItem( WIDTH/2, 260, self.font, "Exit", "exit_game" ))
+        self.titletext.add(Text(
+            WIDTH/2, 65,        # x & y positions
+            self.font,          # font object
+            TITLE,              # text string
+            COLOURS['text'],    # text colour
+            centering=True      # if text is centered
+        ))
+        self.menu.add(MenuItem(
+            WIDTH/2, 130,       # x & y positions
+            self.font,          # font object
+            "Start",            # button text
+            "start_game",       # button target
+            hovered=True        # button is initially in hovered state
+        ))
+        self.menu.add(MenuItem(
+            WIDTH/2, 195,       # x & y positions
+            self.font,          # font object
+            "Options",          # button text
+            "options_menu"      # button target
+        ))
+        self.menu.add(MenuItem(
+            WIDTH/2, 260,       # x & y positions
+            self.font,          # font object
+            "Exit",             # button text
+            "exit_game"         # button target
+        ))
 
     def key_action(self, key):
         if self.state == 0:
@@ -237,7 +286,7 @@ class Game(object):
                     menulist.append(button.hovered)
                 if len(menulist) > 1:
                     for i, val in enumerate(menulist):
-                        if val == True:
+                        if val is True:
                             menulist[i] = False
                             if i-self.movekeys[key][1] > len(menulist)-1:
                                 menulist[0] = True
@@ -245,25 +294,23 @@ class Game(object):
                                 menulist[i-self.movekeys[key][1]] = True
                             break
                     for i, button in enumerate(self.menu):
-                        if menulist[i]: button.hover()
-                        elif button.hovered: button.normal()
+                        if menulist[i]:
+                            button.hover()
+                        elif button.hovered:
+                            button.normal()
 
     def input(self):
         self.mouse.rect.center = pygame.mouse.get_pos()
-
-        collisions = pygame.sprite.spritecollide(self.mouse, self.menu, 0)
 
         for event in pygame.event.get():
 
             # get keyboard presses, better for multiple keys at once
             pressed_keys = pygame.key.get_pressed()
 
-
-            if event.type == QUIT: 
+            if event.type == QUIT:
                 exit()
 
-            #if event.type == MOUSEBUTTONDOWN: #get mouse clicks 
-
+            #if event.type == MOUSEBUTTONDOWN: #get mouse clicks
 
             if self.state > 1:
                 x, y = 0, 0
@@ -276,10 +323,10 @@ class Game(object):
                     for tile in self.ground:
                         tile.move(x, y)
 
-            if event.type == KEYDOWN: #one way of getting keyboard presses
+            if event.type == KEYDOWN:  # one way of getting keyboard presses
                 self.key_action(event.key)
 
-            if event.type == USEREVENT+1: #use this to catch timers
+            if event.type == USEREVENT+1:  # use this to catch timers
                 pass
 
     def draw(self):
